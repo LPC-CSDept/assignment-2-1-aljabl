fe9a7d26728048c2bf6e2602ba01ac32ac5185f8
aljabl <alexandrablais@icloud.com>
2023-06-19 20:59:33 -0700

    attempt1 getting assertion error

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 def main():
-    """
-    ##################################################
-    Comlete your code here
-    Use m_perc and f_perc for your results
-    ##################################################
-    """
+   #asks the user for the number of males and females registered in a class
+    m_perc = int(input('How many students are male? '))
+    f_perc = int(input('How many students are female? '))
+
+    #give total number of students
+    print('The total number of students: ', m_perc + f_perc)
+
+    #give the number of males and females
+    print('The number of males and females: ', m_perc, "\t",  f_perc)
+
+    #give the percentage of males and females
+    print(f'The percentage of males and females: {m_perc:.2f}% \t {f_perc:.2f}%')
 
     """
     ########################################
