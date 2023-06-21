b6b75b3ee6c903a398440c35044371a9de04aa41
aljabl <alexandrablais@icloud.com>
2023-06-21 12:27:02 -0700

    fixed assignment 1

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
 def main():
    #asks the user for the number of males and females registered in a class
-    m_perc = int(input('How many students are male? '))
-    f_perc = int(input('How many students are female? '))
+    males = int(input('How many students are male? '))
+    females = int(input('How many students are female? '))
 
     #give total number of students
-    print('The total number of students: ', m_perc + f_perc)
+    total_students = males + females
+    print('The total number of students: ', total_students)
 
     #give the number of males and females
-    print('The number of males and females: ', m_perc, "\t",  f_perc)
+    print('The number of males and females: ', str(males), '\t', str(females))
+
+    #assign the percentage of males and females to variables
+    m_perc = 100 * males / total_students
+    f_perc = 100 * females / total_students 
 
     #give the percentage of males and females
     print(f'The percentage of males and females: {m_perc:.2f}% \t {f_perc:.2f}%')
